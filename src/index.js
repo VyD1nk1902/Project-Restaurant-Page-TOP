39d630e7590ca139dceaba24316ab42d813410b4
VyDink <vy.dinh.2929@gmail.com>
2025-06-06 13:37:56 +0700

    Add a function setActive for buttons when clickd on

--- src/index.js
+++ src/index.js
@@ -6,22 +6,32 @@ import loadAbout from "./pages/about";
 
 console.log("Miss Thư");
 
+const btns = document.querySelectorAll(".btn");
 const home = document.querySelector(".home");
 const menu = document.querySelector(".menu");
 const about = document.querySelector(".about");
 
-document.addEventListener("DOMContentLoaded", () => {
+function setActiveButton(activeBtn) {
+  btns.forEach((btn) => btn.classList.remove("active"));
+  activeBtn.classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
   loadHomepage();
+  setActiveButton(e.currentTarget);
 });
 
-home.addEventListener("click", () => {
+home.addEventListener("click", (e) => {
   loadHomepage();
+  setActiveButton(e.currentTarget);
 });
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", (e) => {
   loadMenu();
+  setActiveButton(e.currentTarget);
 });
 
-about.addEventListener("click", () => {
+about.addEventListener("click", (e) => {
   loadAbout();
+  setActiveButton(e.currentTarget);
 });
